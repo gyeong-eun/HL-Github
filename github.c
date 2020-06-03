e83aaf43ab6c87d7e6362f43ebbc8e848b9f8722
gyeong-eun <ruddmsdl080@naver.com>
2020-06-03 20:32:51 +0900

    계산프로그램

--- github.c
+++ github.c
@@ -1,7 +1,19 @@
-#include<stdio.com>
+/*#include<stdio.com>
 
 int main()
 {
    printf("hellow world");
    return 0;
+}*/
+#include<stdio.h>
+
+int main()
+{
+	int x,y,z,res;
+	scanf("%d",&x);
+	scanf("%d",&y);
+	scanf("%d",&z);
+    res=((x+y)*z)/x;
+	printf("%d",res);
+	return 0;
 }
